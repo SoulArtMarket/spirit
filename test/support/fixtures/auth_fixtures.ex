6181fd3dceedd4d6381340cb71c649f77fd25f57
cjayross <calvinjayross@gmail.com>
2022-03-03 00:31:55 -0600

    fixed test cases

--- test/support/fixtures/auth_fixtures.ex
+++ test/support/fixtures/auth_fixtures.ex
@@ -4,16 +4,14 @@ defmodule Spirit.AuthFixtures do
   entities via the `Spirit.Auth` context.
   """
 
-  @doc """
-  Generate a token.
-  """
-  def token_fixture(attrs \\ %{}) do
-    {:ok, token} =
-      attrs
-      |> Enum.into(%{})
-      |> Spirit.Auth.create_token()
+  def unique_challenge_pubkey do
+    :crypto.strong_rand_bytes(32)
+    |> Base58.encode58()
+  end
 
-    token
+  def unique_challenge_message do
+    :crypto.strong_rand_bytes(16)
+    |> Base.encode64()
   end
 
   @doc """
@@ -22,7 +20,10 @@ defmodule Spirit.AuthFixtures do
   def challenge_fixture(attrs \\ %{}) do
     {:ok, challenge} =
       attrs
-      |> Enum.into(%{})
+      |> Enum.into(%{
+        pubkey: unique_challenge_pubkey(),
+        message: unique_challenge_message()
+      })
       |> Spirit.Auth.create_challenge()
 
     challenge
